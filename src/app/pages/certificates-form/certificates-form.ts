9b8c0ad03a50857f751b6bbdd72b57e63d082314
kayza <kayzakizy@gmail.com>
2025-09-09 19:47:34 -0300

    add new validation on form

--- src/app/pages/certificates-form/certificates-form.ts
+++ src/app/pages/certificates-form/certificates-form.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
 import { PrimaryButton } from '../../_components/primary-button/primary-button';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificates-form',
-  imports: [SecondaryButton, PrimaryButton],
+  imports: [SecondaryButton, PrimaryButton, FormsModule, CommonModule],
   templateUrl: './certificates-form.html',
   styleUrl: './certificates-form.css',
 })
-export class CertificatesForm {}
+export class CertificatesForm {
+  name: string = '';
+  activity: string = '';
+  listActivity: string[] = ['Arroz', 'Fijão'];
+
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched && control.errors?.['required'];
+  }
+}
